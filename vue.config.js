38147bc67c9337652f607818e6e4e49718a8063c
TheLastDark <yinghuo68@163.com>
2019-03-29 14:54:27 +0800

    moke 数据（init）

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,9 @@
+const addData = require('./data.json')
+const seller = addData.seller
+const goods = addData.goods
+const ratings = addData.ratings
 module.exports = {
+  lintOnSave: false,
   css: {
     loaderOptions: {
       stylus: {
@@ -13,6 +18,28 @@ module.exports = {
     'cube-ui': {
       postCompile: true,
       theme: true
+    }
+  },
+  devServer: {
+    before(app) {
+      app.get('/api/seller', function(req, res) {
+        res.json({
+          errno: 0,
+          data: seller
+        })
+      })
+      app.get('/api/goods', function(req, res) {
+        res.json({
+          errno: 0,
+          data: goods
+        })
+      })
+      app.get('/api/ratings', function(req, res) {
+        res.json({
+          errno: 0,
+          data: ratings
+        })
+      })
     }
   }
 }
