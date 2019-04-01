c54f5d3c2b81935399a5af7c202f2f57ad480904
TheLastDark <yinghuo68@163.com>
2019-04-01 18:12:40 +0800

    ratings

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -78,7 +78,7 @@
   import { getGoods } from 'api'
   import CartControl from 'components/cart-control/cart-control'
   import ShopCart from 'components/shop-cart/shop-cart'
-  // import Food from 'components/food/food'
+  import Food from 'components/food/food'
   import SupportIco from 'components/support-ico/support-ico'
   import Bubble from 'components/bubble/bubble'
 
@@ -145,11 +145,11 @@
           })
         }
       },
-      // selectFood(food) {
-      //   this.selectedFood = food
-      //   this._showFood()
-      //   this._showShopCartSticky()
-      // },
+      selectFood(food) {
+        this.selectedFood = food
+        this._showFood()
+        this._showShopCartSticky()
+      },
       onAdd(target) {
         this.$refs.shopCart.drop(target)
       },
